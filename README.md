3bdfe92cf2a9ead21f86be697b74593fb9f046ce
Lokianer <Lokianer@gmail.com>
2022-12-22 15:04:25 +0100

    The Bot

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # InstagramDiscordPoster
 A Python script, that searches the new post of a specific Instagram Account and post it on a specific Discord Channel.
+
+## Requirements
+- Python 3.10 or higher
+- (instaloader)[https://instaloader.github.io/]
+- (discord.py)[https://discordpy.readthedocs.io/en/stable/]
+- python-dotenv
+- A Discord Bot Token
+- An Instagram Account
+- The old_url.txt file is needed for the script to work. It has to be in the same directory as the script. It cannot be empty, that is why I added a String.
+
+## Installation
+1. Install Python 3.10 or higher
+2. Install instaloader, discord.py and python-dotenv via pip
+3. Create a (Discord Bot)[https://discord.com/developers/applications] and copy the Token
+4. Create a (Instagram Account)[https://www.instagram.com/accounts/emailsignup/] and copy the Username and Password
+5. Create a .env file and add the following lines:
+INSTAGRAM_USERNAME='THE_USERNAME_OF_YOUR_INSTAGRAM_ACCOUNT'
+INSTAGRAM_PASSWORD='THE_PASSWORD_OF_YOUR_INSTAGRAM_ACCOUNT'
+USER_TO_SCRAPE='THE_USERNAME_OF_THE_INSTAGRAM_ACCOUNT_YOU_WANT_TO_SCRAPE'
+DISCORD_TOKEN='THE DISCORD BOT TOKEN'
+6. Add the Channel ID of the Discord Channel you want to post the Instagram Posts to the script. Line 50. (channel = client.get_channel(CHANNEL_ID)) (How to get the Channel ID)[https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-] (For some reason, the Channel ID does not work in the .env file)
+7. Run the script
+
+Feel free to report any bugs or issues.
---
+++ bot.py
@@ -0,0 +1,72 @@
+# Importing Libraries
+import instaloader
+import discord
+from discord.ext import tasks
+import os
+from dotenv import load_dotenv
+load_dotenv()
+bot = instaloader.Instaloader()
+# use .env file to store your credentials
+bot.login(os.getenv('INSTAGRAM_USERNAME'), os.getenv('INSTAGRAM_PASSWORD'))
+# The class that will be used to create the Dicord Client
+class MyClient(discord.Client):
+    # The constructor of the class
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+    # The function that will be called when the bot is ready
+    async def on_ready(self):
+        print('Logged on as', self.user)
+    # A hook that will be called when the bot is ready
+    async def setup_hook(self) -> None:
+        self.check_for_new_post.start()
+    # This function will be called every 5 minutes. You can change the time by changing the seconds parameter#
+    # This will check if there is a new post and if there is, it will send a message to the channel you want
+    @tasks.loop(seconds=300)
+    async def check_for_new_post(self):
+        profile = instaloader.Profile.from_username(bot.context, os.getenv('USER_TO_SCRAPE'))
+        posts = profile.get_posts()
+        post = next(posts)
+   
+        post_details = {}
+        post_details['date'] = post.date
+        post_details['username'] = post.owner_username
+        post_details['caption'] = post.caption
+        post_details['hashtags'] = post.caption_hashtags
+        post_details['media'] = post.url
+        post_details['shortcode'] = post.shortcode
+        # You can modify this part if you want to use a database instead of a file, but I think it's easier to use a file
+        file = open('old_url.txt', 'r')
+        old_url = file.read()
+        file.close()
+        
+        if old_url != post_details['media'] : 
+            file = open('old_url.txt', 'w')
+            file.write(post_details['media'])
+            file.close()    
+            the_posts = post_details
+            print('new post')
+            # You can change the channel id to the channel you want
+            channel = client.get_channel()
+            # The embed that will be sent
+            # You can change the color of the embed by changing the value of the color parameter
+            embed=discord.Embed(title="New Post by "+ the_posts['username'], url="https://instagram.com/"+the_posts['username'], description=the_posts['caption'], color=0xdb2777)
+            embed.set_author(name='Link to Post', url="https://instagram.com/p/"+the_posts['shortcode'])
+            embed.set_thumbnail(url="https://allfacebook.de/wp-content/uploads/2020/02/ig-logo-normal.png")
+            embed.set_image(url=the_posts['media'])
+            embed.add_field(name="date", value=the_posts['date'], inline=True)
+            await channel.send(embed=embed)
+        else:
+            print('no new post')
+    # This function will be called before the loop starts
+    # Just a security measure to make sure the bot is ready before the loop starts
+    @check_for_new_post.before_loop
+    async def before_check_for_new_post(self):
+        await self.wait_until_ready()
+        print('finished waiting')
+# Creating the client
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+# add the token of your bot
+client.run(os.getenv('DISCORD_TOKEN'))
